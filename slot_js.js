5e31d252b14f0b98e18618c77ad1092eca8a6840
Matt Behrens <askedrelic@gmail.com>
2010-02-26 19:39:08 -0500

    Cleanup of function formatting

--- slot_js.js
+++ slot_js.js
@@ -9,7 +9,7 @@ function updateLever() {
     ctx.clearRect(0,0,canvasEl.width, canvasEl.height);
     ctx.fillStyle = 'rgba(0,0,0,0.4)';
 
-    //this was left in from working with lines. not sure how it works, but
+    //this was left in from working with lines.
     //it adds a nice outline effect
     ctx.beginPath();
     ctx.moveTo(d1.left, d1.top);
@@ -21,35 +21,46 @@ function updateLever() {
     ctx.stroke();
 };
 
+function handleSpin() {
+    for(var i = 0; i < 5; i++) {
+        var timeout = i*500;
+        setTimeout(function () { spin("#spin1") }, timeout);
+        setTimeout(function () { spin("#spin2") }, timeout);
+        setTimeout(function () { spin("#spin3") }, timeout);
+    };
+
+};
+
 function spin(div) {
     var spinLoc = $(div).offset();
     var newDiv = $("<div style='position:absolute; float:left; z-index: 100;'> lololol </div>");
-    newDiv
-        .hide()
+    newDiv.hide()
         .css('top', spinLoc.top+10)
         .css('left', spinLoc.left+10);
     $("#spin1").before(newDiv);
-    newDiv.slideToggle(300).animate({top:spinLoc.top+75}, 600).slideUp(300);
+    //TODO: this needs to be converted into an overlay
+    newDiv
+        .slideToggle(300)
+        .animate({top:spinLoc.top+75}, 600, 'swing')
+        .slideUp(300);
     setTimeout(function () {newDiv.remove() }, 2500);
 };
 
+//Setup all main events
 $(function() {
     //manually set contain because Safari wasn't listening
     var l = $("#lever").offset();
     var contain = [l.left, l.top, l.left+30, l.top+270];
     $("#ball").draggable({ axis: 'y' , cursor: 'move', containment: contain});
+
+    //When you drop the hammer (lever).
     $("#sweetspot").droppable({
         drop: function(event, ui) {
-            spin("#spin1");
-            spin("#spin2");
-            spin("#spin3");
-            //for(i = 0; i<4000; i+1100) {
-                //setTimeout(function () {spin("#spin1") }, i);
-            //};
+            handleSpin();
         }
     });
 
-
+    //When you drag the lever, have the canvas update
     $("#ball").bind('drag', function (event,ui) {
         updateLever();
     });
@@ -65,5 +76,6 @@ $(function() {
 
     });
 
+    //Initial draw of the lever
     updateLever();
 });
